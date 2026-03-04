fb1f002972e4b9dca990d12be4721bf7bfffeea6
Brijesh Kushwaha <brijeshkushwaha386@gmail.com>
2026-03-04 19:45:35 +0530

    fix: ThemeToggle import path

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 
 import { ToastProvider } from "./context/ToastContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
